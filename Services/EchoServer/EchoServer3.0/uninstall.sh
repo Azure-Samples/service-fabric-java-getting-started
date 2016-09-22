32c496f626679f0d239c383a3d3375258386b530
Raunak Pandya <raunakp@microsoft.com>
2016-09-22 19:17:22 +0530

    Removing EchoServer3.0

--- Services/EchoServer/EchoServer3.0/uninstall.sh
+++
@@ -1,4 +0,0 @@
-azuresfcli servicefabric application remove fabric:/EchoServerApplication
-azuresfcli servicefabric application type unregister EchoServerApplicationType 3.0.0
-azuresfcli servicefabric application type unregister EchoServerApplicationType 2.0.0
-azuresfcli servicefabric application type unregister EchoServerApplicationType 1.0.0
