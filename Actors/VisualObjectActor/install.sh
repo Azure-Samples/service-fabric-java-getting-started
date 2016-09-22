43b0e8075ee799e0c7ecaa994480ba54f0d68c8c
Raunak Pandya <raunakp@microsoft.com>
2016-09-22 16:24:18 +0000

    Fixing the executable permissions on install/uninstall scripts

--- Actors/VisualObjectActor/install.sh
+++ Actors/VisualObjectActor/install.sh
@@ -3,13 +3,13 @@ azuresfcli servicefabric application type register --application-type-build-path
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied, proceed with default instanceCount of 1"
-    azuresfcli servicefabric application create --application-name fabric:/VisualObjectApp  --application-type-name VisualObjectsApplication --application-type-version 1.0.0
+    azuresfcli servicefabric application create --application-name fabric:/VisualObjectApplication --application-type-name VisualObjectsApplicationType --application-type-version 1.0.0
   elif [ $1 = 0 ]
   then
     echo "Onebox environment, proceed with default instanceCount of 1."
-    azuresfcli servicefabric application create --application-name fabric:/VisualObjectApp  --application-type-name VisualObjectsApplication --application-type-version 1.0.0
+    azuresfcli servicefabric application create --application-name fabric:/VisualObjectApplication  --application-type-name VisualObjectsApplicationType --application-type-version 1.0.0
   elif [ $1 = 1 ]
   then
     echo "Multinode env, proceed with default instanceCount of -1"
-    azuresfcli servicefabric application create --application-name fabric:/VisualObjectApp  --application-type-name VisualObjectsApplication --application-type-version 1.0.0 --application-parameter "[{\"key\":\"InstanceCount\",\"value\":\"-1\"}]"
+    azuresfcli servicefabric application create --application-name fabric:/VisualObjectApplication  --application-type-name VisualObjectsApplicationType --application-type-version 1.0.0 --application-parameter "[{\"key\":\"InstanceCount\",\"value\":\"-1\"}]"
 fi
