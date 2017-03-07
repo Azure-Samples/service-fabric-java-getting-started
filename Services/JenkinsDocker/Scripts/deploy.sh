d715669375085143873400e3362d96d032ed6a9c
Sayantan Saha <saysa@microsoft.com>
2017-03-07 20:20:09 +0530

    Added Node-name filtering for install script, udpated deploy script

--- Services/JenkinsDocker/Scripts/deploy.sh
+++ Services/JenkinsDocker/Scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-appplicationCount=`azure servicefabric application show "fabric:/aApplication" | grep fabric:/aApplication | wc -l`
+appplicationCount=`azure servicefabric application show "fabric:/jenkinsSF" | grep fabric:/jenkinsSF | wc -l`
 if [[ "$appplicationCount" -eq "0" ]];then
     echo "Deploying Application"
     /bin/bash Scripts/install.sh
