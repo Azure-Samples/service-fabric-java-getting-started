78217952092f5fab24c754ca01b68185807cb4d4
Sayantan Saha <saysa@microsoft.com>
2017-03-07 20:42:36 +0530

    Minor modification in node-name filtering command

--- Services/JenkinsDocker/Scripts/install.sh
+++ Services/JenkinsDocker/Scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-nodeName=`azure servicefabric node show | tail -n +3 | head -n -1 | cut -f2-3 -d':' |grep "name" | head -1 | cut -f2 -d':' | tr -d "',"` 
-echo Installing Jenkins Container application on : $nodeName
-azure servicefabric application package copy JenkinsSF fabric:ImageStore
-azure servicefabric application type register JenkinsSF
-azure servicefabric application create fabric:/jenkinsSF JenkinsSFAppType 1.0
-azure servicefabric service create --application-name fabric:/jenkinsSF --service-name fabric:/jenkinsSF/service1 --service-type-name JenkinsOnSFServiceType --instance-count 1 --service-kind Stateless --partition-scheme Singleton --placement-constraints "NodeName == $nodeName" 
+nodeName=`azure servicefabric node show | tail -n +3 | head -n -1 | cut -f2-3 -d':' |grep "name" | head -1 | cut -f2 -d':' | tr -d " ',"` 
+echo Installing Jenkins Container application on :$nodeName
+#azure servicefabric application package copy JenkinsSF fabric:ImageStore
+#azure servicefabric application type register JenkinsSF
+#azure servicefabric application create fabric:/jenkinsSF JenkinsSFAppType 1.0
+#azure servicefabric service create --application-name fabric:/jenkinsSF --service-name fabric:/jenkinsSF/service1 --service-type-name JenkinsOnSFServiceType --instance-count 1 --service-kind Stateless --partition-scheme Singleton --placement-constraints "NodeName == $nodeName" 
