7e560b20eb5b739262c5bbebd0c0ba338d2ef55b
Sayantan Saha <saysa@microsoft.com>
2017-04-18 14:19:48 +0530

    minor

--- Services/EchoServer/EchoServer2.0/Scripts/upgrade.sh
+++ Services/EchoServer/EchoServer2.0/Scripts/upgrade.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -x
 
-azure servicefabric application package copy EchoServerApplication fabric:ImageStore > /dev/null 2>&1
+callerPath=${0}
+if [[ "$callerPath" =~ "Scripts" ]];then
+ appPkg="EchoServerApplication2"
+else
+ appPkg="../EchoServerApplication2"
+fi
+
+azure servicefabric application package copy $appPkg fabric:ImageStore > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Application copy failed."
     exit 1
 fi
-azure servicefabric application type register EchoServerApplication > /dev/null 2>&1
+azure servicefabric application type register EchoServerApplication2 > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Application type registration failed."
     exit 1
 fi
-version=$(sed -e "s/xmlns/ignore/" EchoServerApplication/ApplicationManifest.xml | xmllint --xpath "string(//ApplicationManifest/@ApplicationTypeVersion)" -)
+version=$(sed -e "s/xmlns/ignore/" $appPkg/ApplicationManifest.xml | xmllint --xpath "string(//ApplicationManifest/@ApplicationTypeVersion)" -)
 eval azure servicefabric application upgrade start --application-name fabric:/EchoServerApplication --target-application-type-version ${version} --rolling-upgrade-mode Monitored > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Upgrade of application failed."
