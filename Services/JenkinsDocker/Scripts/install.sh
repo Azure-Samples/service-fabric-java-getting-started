eb38e6ad4e029288a62fe3b81538610f6597b5c2
Sayantan Saha <saysa@microsoft.com>
2017-05-05 15:46:17 +0530

    adding missed line

--- Services/JenkinsDocker/Scripts/install.sh
+++ Services/JenkinsDocker/Scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+azure servicefabric application package copy JenkinsSF fabric:ImageStore
 azure servicefabric application type register JenkinsSF
 azure servicefabric application create fabric:/JenkinsSF JenkinsSFApplicationType 1.0
 azure servicefabric service create --application-name fabric:/JenkinsSF --service-name fabric:/JenkinsSF/ContainerService --service-type-name JenkinsOnSFServiceType --instance-count 1 --service-kind Stateless --partition-scheme Singleton
