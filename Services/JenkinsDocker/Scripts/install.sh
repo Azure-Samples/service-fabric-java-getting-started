c3650d5535e2b7a4dd55ba32b5e59713f1acb7a2
Sayantan Saha <saysa@microsoft.com>
2017-05-05 11:53:21 +0530

    Updated manifest files to handle latest changes in Jenkins docker

--- Services/JenkinsDocker/Scripts/install.sh
+++ Services/JenkinsDocker/Scripts/install.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-#nodeName=`azure servicefabric node show | tail -n +3 | head -n -1 | cut -f2-3 -d':' |grep "name" | head -1 | cut -f2 -d':' | tr -d " '," | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"` 
-#echo Deploying Jenkins container service on :$nodeName
-azure servicefabric application package copy JenkinsSF fabric:ImageStore
+#!/bin/bash -e
 azure servicefabric application type register JenkinsSF
 azure servicefabric application create fabric:/JenkinsSF JenkinsSFApplicationType 1.0
 azure servicefabric service create --application-name fabric:/JenkinsSF --service-name fabric:/JenkinsSF/ContainerService --service-type-name JenkinsOnSFServiceType --instance-count 1 --service-kind Stateless --partition-scheme Singleton
