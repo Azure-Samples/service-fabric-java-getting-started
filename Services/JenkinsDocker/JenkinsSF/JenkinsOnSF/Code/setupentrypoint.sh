25724e01ce42cf764286202d7c8abde6c4a05bbf
Sayantan Saha <saysa@microsoft.com>
2017-05-09 17:40:25 +0530

    some more experiments - latest image with jenkins uid 1003, setup-entrypoint etc

---
+++ Services/JenkinsDocker/JenkinsSF/JenkinsOnSF/Code/setupentrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+user=jenkins
+group=jenkins
+uid=1003
+gid=1003
+sudo groupadd -g ${gid} ${group}
+sudo useradd  -u ${uid} -g ${gid} 
+sudo mkdir -p /mnt/docker_dump/jenkins_home
+sudo chown -R jenkins:jenkins /mnt/docker_dump/jenkins_home
+
+sudo apt-get install -qqy cifs-utils
+sudo mount -t cifs //teststoragess1.file.core.windows.net/tempfileshare /mnt/docker_dump/jenkins_home -o vers=3.0,username=teststoragess1,password=NgYbDlXf3VPZBRUak3VVmU/2eUVVpTjp4DSpgQTanXGNRQyKMWUAmdCoWmwDJZpZoxQk724eqHBhzRQkEUvc3w==,dir_mode=0777,file_mode=0777
+
