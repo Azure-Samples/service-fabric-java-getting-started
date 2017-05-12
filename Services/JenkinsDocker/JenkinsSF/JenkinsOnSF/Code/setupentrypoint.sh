d15286f3114c428e7c0551ae89a8a1524066e004
Sayantan Saha <saysa@microsoft.com>
2017-05-12 16:56:23 +0530

    mounting as jenkins/1003

--- Services/JenkinsDocker/JenkinsSF/JenkinsOnSF/Code/setupentrypoint.sh
+++ Services/JenkinsDocker/JenkinsSF/JenkinsOnSF/Code/setupentrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+
 user=jenkins
 group=jenkins
 uid=1003
@@ -9,5 +10,6 @@ sudo mkdir -p /mnt/docker_dump/jenkins_home
 sudo chown -R jenkins:jenkins /mnt/docker_dump/jenkins_home
 
 sudo apt-get install -qqy cifs-utils
-sudo mount -t cifs //teststoragess1.file.core.windows.net/tempfileshare /mnt/docker_dump/jenkins_home -o vers=3.0,username=teststoragess1,password=NgYbDlXf3VPZBRUak3VVmU/2eUVVpTjp4DSpgQTanXGNRQyKMWUAmdCoWmwDJZpZoxQk724eqHBhzRQkEUvc3w==,dir_mode=0777,file_mode=0777
+sudo mount -t cifs //teststoragess1.file.core.windows.net/tempfileshare /mnt/docker_dump/jenkins_home -o vers=3.0,username=teststoragess1,password=NgYbDlXf3VPZBRUak3VVmU/2eUVVpTjp4DSpgQTanXGNRQyKMWUAmdCoWmwDJZpZoxQk724eqHBhzRQkEUvc3w==,dir_mode=0777,file_mode=0777,uid=${uid},gid=${gid}
+
 
