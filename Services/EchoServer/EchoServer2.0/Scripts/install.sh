6b95de64eea318b698e102d2b1bda760a829b9d4
samedder <samedder@users.noreply.github.com>
2017-08-10 19:19:33 -0700

    Fixing EchoServer2.0 scripts

--- Services/EchoServer/EchoServer2.0/Scripts/install.sh
+++ Services/EchoServer/EchoServer2.0/Scripts/install.sh
@@ -4,8 +4,13 @@ set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 appPkg="$DIR/../EchoServerApplication2"
 
-azure servicefabric application package copy --application-package-path $appPkg --image-store-connection-string fabric:ImageStore
-azure servicefabric application type register --application-type-build-path EchoServerApplication2
+sfctl application upload --path $appPkg --show-progress
+sfctl application provision --application-type-build-path EchoServerApplication2
 
-# Used below command to upgrade from version 1.0 to version 2.0
-azure servicefabric application upgrade start --application-name fabric:/EchoServerApplication --target-application-type-version 2.0.0 --rolling-upgrade-mode Monitored
+# Check parameters to see if InstanceCount was previously specified
+params=$(sfctl application info --application-id EchoServerApplication --query "parameters[].[key, value][]")
+if [[ $params =~ "InstanceCount" ]]; then
+    sfctl application upgrade --app-id EchoServerApplication --app-version 2.0.0 --parameters "{\"InstanceCount\":\"-1\"}" --mode "Monitored"
+else
+    sfctl application upgrade --app-id EchoServerApplication --app-version 2.0.0 --parameters "" --mode "Monitored"
+fi
--- Services/EchoServer/EchoServer2.0/Scripts/uninstall-sfctl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sfctl application delete --application-id fabric:/EchoServerApplication
-sfctl application unprovision --application-type-name EchoServerApplicationType --application-type-version 2.0.0
-sfctl application unprovision --application-type-name EchoServerApplicationType --application-type-version 1.0.0
-sfctl store delete --content-path EchoServerApplication
