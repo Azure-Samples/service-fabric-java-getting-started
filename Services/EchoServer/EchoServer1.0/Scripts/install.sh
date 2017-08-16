ac2ade11a2da8ef26b571556e600743b91e2265d
Sayantan Saha <saysa@microsoft.com>
2017-08-16 15:41:52 +0530

    Fixing typos in the merge from samedder:master

--- Services/EchoServer/EchoServer1.0/Scripts/install.sh
+++ Services/EchoServer/EchoServer1.0/Scripts/install.sh
@@ -5,8 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 appPkg="$DIR/../EchoServerApplication"
 
 sfctl application upload --path $appPkg --show-progress
-sfctl application provision --application-type-build-path CounterActorApplication
-sfctl application create --app-name fabric:/CounterActorApplication --app-type EchoServerApplication --app-version 1.0.0
+sfctl application provision --application-type-build-path EchoServerApplication
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied, proceed with default instanceCount of 1"
