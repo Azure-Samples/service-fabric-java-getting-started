43e8fff2799fcc68424c26c2c150c7c077ee625e
Patchigolla <rapatchi@microsoft.com>
2018-04-27 04:14:02 -0700

    Adding help

--- reliable-services-actor-sample/Services/WatchDog/Scripts/install.sh
+++ reliable-services-actor-sample/Services/WatchDog/Scripts/install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-set -x
+
 create_app()
 {
-  if [ $# -eq 0 ]; then
-    sfctl application create --app-name fabric:/WatchdogApplication --app-type WatchdogApplicationType --app-version 1.0.0
-  else
-    sfctl application create --app-name fabric:/WatchdogApplication --app-type WatchdogApplicationType --app-version 1.0.0 --parameters "{\"InstanceCount\":\"-1\"}"
-  fi
+  sfctl application create --app-name fabric:/WatchdogApplication --app-type WatchdogApplicationType --app-version 1.0.0 --parameters $1
 }
+print_help()
+{
+  echo "Additional Options"
+  echo "-onebox (Default): If you are deploying application on one box cluster"
+  echo "-multinode: If you are deploying application on a multi node cluster"
+}
+
+if [ "$1" = "--help" ]
+  then
+    print_help
+    exit 0
+fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 appPkg="$DIR/../WatchdogApplication"
 
@@ -17,13 +25,13 @@ sfctl application provision --application-type-build-path WatchdogApplication
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied, proceed with default instanceCount of 1"
-    create_app
-  elif [ $1 = "onebox" ]
+    create_app {}
+  elif [ $1 = "-onebox" ]
   then
     echo "Onebox environment, proceed with default instanceCount of 1."
-    create_app
-  elif [ $1 = "multinode" ]
+    create_app {}
+  elif [ $1 = "-multinode" ]
   then
     echo "Multinode env, proceed with default instanceCount of -1"
-    create_app 1
+    create_app "{\"InstanceCount\":\"-1\"}"
 fi
